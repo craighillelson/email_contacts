670fa1b4af2f5b00a065a75318d97558a1ef32df
craighillelson <craighillelson@gmail.com>
2020-06-08 13:08:10 -0400

    create list_contacts.py
    first commit

---
+++ list_contacts.py
@@ -0,0 +1,35 @@
+"""List contacts."""
+
+import csv
+from collections import namedtuple
+
+
+def open_csv_pop_dct_namedtuple():
+    """Populate a dictionary with the contents of a csv."""
+    dct = {}
+    with open('contacts.csv') as f:
+        f_csv = csv.reader(f)
+        headings = next(f_csv)
+        Row = namedtuple('Row', headings)
+        for r in f_csv:
+            row = Row(*r)
+            dct[row.email] = [row.first_name, row.last_name, row.phone_number]
+
+    return dct
+
+
+def output_contacts(dct):
+    """
+    If the contacts dictionary contains one or more records, loop through
+    the dictionary and print each record. If there are no records, notify the
+    user that the database is empty.
+    """
+    if contacts:
+        for email, details in dct.items():
+            print(email, *details)
+    else:
+        print('The database is empty.')
+
+
+contacts = open_csv_pop_dct_namedtuple()
+output_contacts(contacts)
