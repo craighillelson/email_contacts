5a02c8c5919f3ab5cf324c48a6c94f0384eb284b
Craig Hillelson <craighillelson@gmail.com>
2020-07-06 07:59:57 -0400

    update list_contacts.py
    streamlined code

--- list_contacts.py
+++ list_contacts.py
@@ -1,45 +1,6 @@
 """List contacts."""
 
-import csv
-from collections import namedtuple
+import functions
 
-
-def open_csv_pop_dct_namedtuple():
-    """Populate a dictionary with the contents of a csv."""
-    dct = {}
-    with open('contacts.csv') as f:
-        f_csv = csv.reader(f)
-        headings = next(f_csv)
-        Row = namedtuple('Row', headings)
-        for r in f_csv:
-            row = Row(*r)
-            dct[row.email] = [row.first_name, row.last_name, row.phone_number,
-                              row.city, row.state, row.zip_code, row.active]
-
-    return dct
-
-
-def output_contacts(dct):
-    """
-    If the contacts dictionary contains one or more records, loop through
-    the dictionary and print each record. If there are no records, notify the
-    user that the database is empty.
-    """
-    if contacts:
-        print('\n')
-        for i, (email, details) in enumerate(dct.items(), 1):
-            print(f'record: {i}')
-            print(f'email: {email}')
-            print(f'name: {details[0]} {details[1]}')
-            print(f'phone number: {details[2]}')
-            print(f'city: {details[3]}')
-            print(f'state: {details[4]}')
-            print(f'zip code: {details[5]}')
-            print(f'active: {details[6]}\n')
-    else:
-        print('The database is empty.')
-    print('\n')
-
-
-contacts = open_csv_pop_dct_namedtuple()
-output_contacts(contacts)
+contacts = functions.open_csv_pop_lst_namedtuple()
+functions.output_contacts(contacts)
