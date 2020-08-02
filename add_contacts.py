e41d504d580ff8c55741c8a8fc71d838338dd4ed
craighillelson <craighillelson@gmail.com>
2020-08-02 15:59:38 -0400

    update add_contacts.py
    simplified import statement

--- add_contacts.py
+++ add_contacts.py
@@ -3,17 +3,22 @@ Open a csv and populate a dictionary with its contents. Prompt the user to
 add members.
 """
 
-import functions
+import pyinputplus as pyip
+from functions import (open_csv_pop_lst_namedtuple,
+                       get_domain,
+                       prompt_user_for_domain,
+                       add_user_input_to_csv,)
 
-contacts = functions.open_csv_pop_lst_namedtuple()
+contacts = open_csv_pop_lst_namedtuple()
 if contacts:
-    domain = functions.get_domain('email', contacts)
-    domain_confirmed = functions.prompt_user_for_domain(domain)
-    functions.add_user_input_to_csv(domain, contacts)
+    domain = get_domain('email', contacts)
+    domain_confirmed = prompt_user_for_domain(domain)
+    add_user_input_to_csv(domain, contacts)
 else:
-    add_contacts = pyip.inputYesNo('\nThere are no entries in the csv. Would you like to add contacts?\n> ')
+    add_contacts = pyip.inputYesNo('\nThere are no entries in the csv. '
+                                   'Would you like to add contacts?\n> ')
     if add_contacts == 'yes':
         domain = input('What is your domain?\n> ')
-        functions.add_user_input_to_csv(domain, contacts)
+        add_user_input_to_csv(domain, contacts)
     else:
         pass
