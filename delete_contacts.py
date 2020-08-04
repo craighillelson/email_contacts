a19184101271abe27965bf705c0ccdd9ca52bda7
craighillelson <craighillelson@gmail.com>
2020-08-04 08:36:00 -0400

    update delete_contacts.py
    replaced blanket import functions statement with more targeted from functions import statement

--- delete_contacts.py
+++ delete_contacts.py
@@ -3,12 +3,19 @@ Import a csv, present user with options to remove. Write the results to
 contacts.csv.
 """
 
-import functions
+from functions import (open_csv_pop_lst_namedtuple,
+                       number_original_contacts,
+                       get_list_of_keys,
+                       output_options,
+                       prompt_user_to_remove,
+                       output_remaining_contacts,
+                       write_lst_to_csv,)
 
-original_contacts = functions.open_csv_pop_lst_namedtuple()
-original_contacts_numbered = functions.number_original_contacts(original_contacts)
-nums = functions.get_list_of_keys(original_contacts_numbered)
-functions.output_options(original_contacts_numbered)
-contacts_to_skip = functions.prompt_user_to_remove(nums)
-remaining_members = functions.output_remaining_contacts(original_contacts_numbered, contacts_to_skip)
-functions.write_lst_to_csv(remaining_members)
+original_contacts = open_csv_pop_lst_namedtuple()
+original_contacts_numbered = number_original_contacts(original_contacts)
+nums = get_list_of_keys(original_contacts_numbered)
+output_options(original_contacts_numbered)
+contacts_to_skip = prompt_user_to_remove(nums)
+remaining_members = output_remaining_contacts(original_contacts_numbered,
+                                              contacts_to_skip)
+write_lst_to_csv(remaining_members)
