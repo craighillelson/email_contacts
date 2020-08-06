bf6e2562c4e304f2dd1e53a33af987a6016faeb4
craighillelson <craighillelson@gmail.com>
2020-08-06 00:15:15 -0400

    update edit_contacts.py
    streamlined code by importing only the functions needed

--- edit_contacts.py
+++ edit_contacts.py
@@ -3,11 +3,16 @@ Import a csv, present user with options to skip.
 Write the results to a csv.
 """
 
-import functions
+from functions import (open_csv_pop_lst_namedtuple,
+                       number_original_contacts,
+                       get_list_of_keys,
+                       output_options,
+                       prompt_user,
+                       write_lst_to_csv,)
 
-original_contacts = functions.open_csv_pop_lst_namedtuple()
-original_contacts_numbered = functions.number_original_contacts(original_contacts)
-nums = functions.get_list_of_keys(original_contacts_numbered)
-functions.output_options(original_contacts_numbered)
-remaining_contacts = functions.prompt_user(original_contacts_numbered)
-functions.write_lst_to_csv(remaining_contacts)
+original_contacts = open_csv_pop_lst_namedtuple()
+original_contacts_numbered = number_original_contacts(original_contacts)
+nums = get_list_of_keys(original_contacts_numbered)
+output_options(original_contacts_numbered)
+remaining_contacts = prompt_user(original_contacts_numbered)
+write_lst_to_csv(remaining_contacts)
