4e33a86b3046f92d74d6c634610d46f57069e085
craighillelson <craighillelson@gmail.com>
2020-08-09 09:41:33 -0400

    update menu.py
    added search.py as an option

--- menu.py
+++ menu.py
@@ -3,10 +3,11 @@
 import pyinputplus as pyip
 
 options = {
-    1: 'add_contacts.py',
-    2: 'delete_contacts.py',
-    3: 'edit_contacts.py',
-    4: 'list_contacts.py'
+    1: "add_contacts.py",
+    2: "delete_contacts.py",
+    3: "edit_contacts.py",
+    4: "list_contacts.py",
+    5: "search_contacts.py"
     }
 
 
@@ -17,12 +18,12 @@ def switch_case(argument):
 
 
 while True:
-    print('\nMake a selection or press enter to quit')
+    print("\nMake a selection or press enter to quit")
     for num, option in options.items():
         print(num, option)
-    selection = pyip.inputInt('> ', min=1, max=len(options), blank=True)
-    if selection != '':
+    selection = pyip.inputInt("> ", min=1, max=len(options), blank=True)
+    if selection != "":
         exec(open(switch_case(selection)).read())
     else:
-        print('\nSession complete.\n')
+        print("\nSession complete.\n")
         break
