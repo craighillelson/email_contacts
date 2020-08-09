31761b3d0ff586d4fa695eeae85af1eeeeaf456f
craighillelson <craighillelson@gmail.com>
2020-08-09 09:39:39 -0400

    update edit_contacts.py
    added logic to handle cases where user doesn't make any edits

--- edit_contacts.py
+++ edit_contacts.py
@@ -12,7 +12,12 @@ from functions import (open_csv_pop_lst_namedtuple,
 
 original_contacts = open_csv_pop_lst_namedtuple()
 original_contacts_numbered = number_original_contacts(original_contacts)
+
 nums = get_list_of_keys(original_contacts_numbered)
 output_options(original_contacts_numbered)
-remaining_contacts = prompt_user(original_contacts_numbered)
-write_lst_to_csv(remaining_contacts)
+
+updated_contacts = prompt_user(original_contacts_numbered)
+if updated_contacts:
+    write_lst_to_csv(updated_contacts)
+else:
+    print("no changes made")
