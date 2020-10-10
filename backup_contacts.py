e1f82e4d08d8a559463951b71866f97c9f0c24b6
Craig Hillelson <craighillelson@gmail.com>
2020-10-10 08:03:47 -0400

    update backup_contacts.py
    linted code

--- backup_contacts.py
+++ backup_contacts.py
@@ -3,7 +3,7 @@
 from functions import backup_contacts, create_timestamped_filename
 
 backup_filename = create_timestamped_filename()
-directory = "backup"
-path = directory + "/" + backup_filename
+DIRECTORY = "backup"
+path = DIRECTORY + "/" + backup_filename
 backup_contacts(path)
 print(f"\ncontacts.csv was copied to {path}")
